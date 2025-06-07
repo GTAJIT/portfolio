e53067e21381188df041b0a6ef9352cd4c091cf5
GTAJIT <mukhujeegtajit@gmail.com>
2025-06-07 23:20:18 +0530

    code: Contact page ++

---
+++ src/components/SocialIcons.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { getPics } from '../hooks/useSocials';
+import { useState,useEffect } from 'react';
+import {PopUp} from "./ui/ui"
+
+export default function SocialIcons() {
+    const [data, setData] = useState([])
+        useEffect(()=>{
+            const fetchProjects = async () => {
+                const p = await getPics();
+                setData(p);
+            };
+            fetchProjects();
+        }, []);
+    
+  return (
+    <>
+      <div className='flex gap-5 items-center'>
+        {data.map((item, index)=>(
+          <div key={index} className='flex items-center gap-2 my-2'>
+            <a
+              href={item.slug}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cursor-pointer hover:fill-blue-600 scale-[1.5] hover:scale-[1.7]"
+            >
+              <PopUp name={item.name}>
+                <img src={item.logo} alt={item.name} className="w-6 h-6" />
+              </PopUp>
+            </a>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import {SocialIcons} from "../components/component"
 
 export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <>
+      <div className="h-screen flex items-center justify-between">
+        <div className="flex flex-col justify-between">
+          <div className="">
+            <p><SocialIcons/></p>
+            <span>
+              <h1>Get in Touch</h1>
+              <p>
+                Have questions or ready to transform your business idea with AI
+                automation ?
+              </p>
+            </span>
+          </div>
+          <div className="">
+            <span>email</span>
+            <span>no</span>
+          </div>
+        </div>
+        <div className="">form</div>
+      </div>
+    </>
+  );
 }
