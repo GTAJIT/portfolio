c2b30c1a25bfa05ca123f5e39d940982cd39965f
GTAJIT <mukhujeegtajit@gmail.com>
2025-06-07 11:47:01 +0530

    code: Navbar Ready

---
+++ src/components/component.js
@@ -0,0 +1,8 @@
+import Navbar from "./Navbar";
+import Container from "./header/Container";
+import Logo from "./header/Logo";
+import ProjectsCard from "./Projects-card"
+import Projects from "./Projects"
+import Footer from "./Footer";
+
+export {Logo, Container, Navbar, Footer, Projects, ProjectsCard}
---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <div className="cursor-pointer">CodesByJit</div>
+  )
+}
