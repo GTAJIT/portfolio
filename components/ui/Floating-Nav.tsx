42af9f7e864a30ef4c76d668b98062be75ff5f4e
GTAJIT <mukhujeegtajit@gmail.com>
2025-07-12 09:52:22 +0530

    dependencies fixed

--- components/ui/Floating-Nav.tsx
+++ components/ui/Floating-Nav.tsx
@@ -58,7 +58,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit md:min-w-[70vw] lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto px-4 py-2 rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center space-x-4",
+          "flex max-w-fit sm:space-x-1 sm:min-w-fit md:min-w-fit lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto px-4 py-2 rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center space-x-4",
           className
         )}
         style={{
@@ -73,7 +73,7 @@ export const FloatingNav = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 items-center  flex space-3 text-neutral-600 dark:hover:text-neutral-300 dark:hover:bg-black-100 px-6 py-2 rounded-xl hover:text-neutral-500"
+              "relative dark:text-neutral-50 items-center flex text-neutral-600 dark:hover:text-neutral-300 dark:hover:bg-black-100 lg:px-6 md:px-4 sm:px-1 py-2 rounded-xl hover:text-neutral-500"
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
