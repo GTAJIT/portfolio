8096ce4c60f0933c4575aa8d7bc8859f08d66171
GTAJIT <mukhujeegtajit@gmail.com>
2025-07-12 09:10:17 +0530

    dependencies fixed

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 text-white flex justify-center items-center flex-col overflow-hidden overflow-clip mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 text-white flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         {/* <Suspense fallback={<Loading/>}> */}
         <FloatingNav navItems={navItems}/>
